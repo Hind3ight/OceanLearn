7175ff5c3e47d812b3f6bcbdae1238379fd3f3c7
Hind3ight <hind3ight@163.com>
2022-02-28 12:50:07 +0800

    login

--- controller/user.go
+++ controller/user.go
@@ -41,17 +41,65 @@ func Register(c *gin.Context) {
 		})
 		return
 	} else {
-		user := model.User{
+		newUser := model.User{
 			Name:      name,
 			Telephone: telephone,
 			Password:  password,
 		}
-		DB.Create(&user)
+		DB.Create(&newUser)
 	}
 
 	c.JSON(200, gin.H{
 		"message": "注册成功",
 		"name":    name,
+	})
+}
+
+func Login(c *gin.Context) {
+	DB := common.GetDB()
+	telephone := c.PostForm("telephone")
+	password := c.PostForm("password")
+
+	if len(telephone) != 11 {
+		c.JSON(http.StatusUnprocessableEntity, gin.H{
+			"code":    402,
+			"message": "手机号必须为11位",
+		})
+		return
+	}
+
+	if len(password) < 6 {
+		c.JSON(http.StatusUnprocessableEntity, gin.H{
+			"code":    402,
+			"message": "密码需大于6位",
+		})
+		return
+	}
+
+	var user model.User
+	DB.Where("telephone = ?", telephone).First(&user)
+	if user.ID == 0 {
+		c.JSON(http.StatusUnprocessableEntity, gin.H{
+			"code":    422,
+			"message": "用户不存在",
+		})
+		return
+	}
+
+	if user.Password != password {
+		c.JSON(http.StatusUnprocessableEntity, gin.H{
+			"code":    422,
+			"message": "密码错误",
+		})
+		return
+	}
+
+	token := "11"
+
+	c.JSON(200, gin.H{
+		"code":    200,
+		"data":    gin.H{"token": token},
+		"message": "登录成功",
 	})
 }
 
